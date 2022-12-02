22003bb156df8b8d2e63f7269a128d258cd7ae7f
Ronald1981 <41762305+Ronald1981@users.noreply.github.com>
2022-12-02 17:47:05 -0400

    Create 04_Consultas.sql

---
+++ 04_Consultas.sql
@@ -0,0 +1,11 @@
+USE universidad;
+
+show tables;
+
+select * from personas;
+
+select CodPersona, CI, Nombre, A_primer from personas;
+
+SELECT * FROM universidad.dato1;
+
+SELECT * FROM universidad.dato;
